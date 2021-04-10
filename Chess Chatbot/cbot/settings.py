2910288c5b3c03d332758ba2877a3132092d804b
RahulR100 <rahulrao@email.com>
2021-04-10 10:52:02 -0400

    added django framework

--- Chess Chatbot/cbot/settings.py
+++ Chess Chatbot/cbot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o(w00_da%e%f5m$tnr6maku6xk_r1zy*9$x=dlfc7lu3(@*(x-'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
